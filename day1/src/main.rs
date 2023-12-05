78cb7d8842b24147e0f9f8f83e831b1d0d73a4ec
Drawserqzez <me@holton.lol>
2023-12-05 17:21:13 +0100

    Work on part 2 of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,13 +5,11 @@ fn main() {
     let mut sum = 0;
 
     for i in input {
-        sum += process(&i);
+        sum += process_part_one(&i);
     }
 
     print!("Sum: {}", sum);
 }
-
-        //vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
 
 fn read_from_file() -> Vec<String> {
     let mut res = Vec::new();
@@ -23,7 +21,41 @@ fn read_from_file() -> Vec<String> {
     res
 }
 
-fn process(line: &str) -> i32 {
+fn is_valid_number(input: &str) -> bool {
+    match input {
+        "one" | "1" => true,
+        "two" | "2" => true,
+        "three" | "3" => true,
+        "four" | "4" => true,
+        "five" | "5" => true,
+        "six" | "6" => true,
+        "seven" | "7" => true,
+        "eight" | "8" => true,
+        "nine" | "9" => true,
+        _ => false
+    }
+}
+
+fn get_string_number(s: &str) -> Option<i32> {
+    match s.to_lowercase().as_str() {
+        "one" | "1" => Some(1),
+        "two" | "2" => Some(2),
+        "three" | "3" => Some(3),
+        "four" | "4" => Some(4),
+        "five" | "5" => Some(5),
+        "six" | "6" => Some(6),
+        "seven" | "7" => Some(7),
+        "eight" | "8" => Some(8),
+        "nine" | "9" => Some(9),
+        _ => None
+    }
+}
+
+fn process_part_two(line: &str) -> i32 {
+
+}
+
+fn process_part_one(line: &str) -> i32 {
     let digits: Vec<char> = line.chars().filter(|x| x.is_numeric()).collect();
 
     let first = digits.first().unwrap();
