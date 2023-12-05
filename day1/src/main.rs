e873d3c4767c09094514a54d8353bc7d8d7f3992
Drawserqzez <me@holton.lol>
2023-12-05 14:24:12 +0100

    Day 1: Solved

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,36 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_from_file();
+    let mut sum = 0;
+
+    for i in input {
+        sum += process(&i);
+    }
+
+    print!("Sum: {}", sum);
+}
+
+        //vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+fn read_from_file() -> Vec<String> {
+    let mut res = Vec::new();
+
+    for line in read_to_string("../input.txt").unwrap().lines().map(String::from) {
+        res.push(line);
+    }
+
+    res
+}
+
+fn process(line: &str) -> i32 {
+    let digits: Vec<char> = line.chars().filter(|x| x.is_numeric()).collect();
+
+    let first = digits.first().unwrap();
+    let last = digits.last().unwrap();
+
+    let combined = format!("{}{}", first, last);
+
+    combined.parse::<i32>().unwrap()
 }
 
